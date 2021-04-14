e44d2aae52825a765a46c953be1febeb7bd8068a
Tatu Saloranta <tatu.saloranta@iki.fi>
2021-04-14 14:58:31 -0700

    First version with lots of fail :)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# jackson-jdk16-compat-test
-Test suite for JDK 16 compatibility of Jackson components, mainly jackson-databind
+# Overview
+
+Test suite containing unit tests that either require Java 16 (and up) features (JDK 16+)
+or exercise features that behave differently on Java 16 platform (for example due
+to stricter visibility restrictions).
+
+## Status
+
+[![Build Status](https://travis-ci.org/FasterXML/jackson-jdk16-compat-test.svg)](https://travis-ci.org/FasterXML/jackson-jdk16-compat-test)
---
+++ src/test/java/com/fasterxml/jackson/jdk16tests/Jdk16TestBase.java
@@ -0,0 +1,59 @@
+package com.fasterxml.jackson.jdk16tests;
+
+import java.util.Arrays;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
+public class Jdk16TestBase
+    extends junit.framework.TestCase
+{
+    /*
+    /**********************************************************************
+    /* Factory methods (simple)
+    /**********************************************************************
+     */
+
+    protected static ObjectMapper newJsonMapper() {
+        return new JsonMapper();
+    }
+
+    protected static JsonMapper.Builder jsonMapperBuilder() {
+        return JsonMapper.builder();
+    }
+
+    /*
+    /**********************************************************************
+    /* Assertion methods
+    /**********************************************************************
+     */
+
+    public static void verifyException(Throwable e, String... matches)
+    {
+        String msg = e.getMessage();
+        String lmsg = (msg == null) ? "" : msg.toLowerCase();
+        for (String match : matches) {
+            String lmatch = match.toLowerCase();
+            if (lmsg.indexOf(lmatch) >= 0) {
+                return;
+            }
+        }
+        fail("Expected an exception with one of substrings ("
+                +Arrays.asList(matches)+"): got one (of type "+e.getClass().getName()
+                +") with message \""+msg+"\"");
+    }
+    
+    /*
+    /**********************************************************************
+    /* Other helper methods
+    /**********************************************************************
+     */
+
+    public String q(String str) {
+        return '"'+str+'"';
+    }
+
+    protected static String a2q(String json) {
+        return json.replace("'", "\"");
+    }
+}

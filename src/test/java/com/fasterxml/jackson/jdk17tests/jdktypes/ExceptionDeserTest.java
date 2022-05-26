1ba99dce305bcc78cef5ca3473cef6e08d9bccf7
Tatu Saloranta <tatu.saloranta@iki.fi>
2022-05-26 16:36:33 -0700

    Add a test for DB/3275 (passing with 2.13.4-SNAPSHOT)

--- src/test/java/com/fasterxml/jackson/jdk17tests/jdktypes/ExceptionDeserTest.java
+++ src/test/java/com/fasterxml/jackson/jdk17tests/jdktypes/ExceptionDeserTest.java
@@ -1,5 +1,8 @@
 package com.fasterxml.jackson.jdk17tests.jdktypes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.jdk17tests.Jdk17TestBase;
 
 public class ExceptionDeserTest extends Jdk17TestBase
@@ -7,6 +10,12 @@ public class ExceptionDeserTest extends Jdk17TestBase
     // for [databind#3275]
     public void testIssue3275() throws Exception
     {
-        // !!! TODO
+        final ObjectMapper mapper = JsonMapper.builder()
+                .propertyNamingStrategy(PropertyNamingStrategies.UPPER_CAMEL_CASE)
+                .build();
+
+        String jsonString = "{\"message\":\"This is my runtime exception message\"}";
+        Exception ex = mapper.readValue(jsonString, Exception.class);
+        assertNotNull(ex);
     }
 }
